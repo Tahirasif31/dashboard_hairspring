f336a9f011fe7e7dbc162514c1fe9fba5b40a252
tahirasif31 <tahirasif31@gmail.com>
2024-02-29 15:07:51 +0500

    navbar completed

--- app/components/sidebar/Sidebar.js
+++ app/components/sidebar/Sidebar.js
@@ -2,6 +2,41 @@ import Image from "next/image";
 import SidebarItem from "./SidbarItem";
 import SidebarList from "./SidebarList";
 
+const SIDEBARITEMS = [
+  {
+    title: "Dashboard",
+    href: "/",
+    img: "/dashboard.svg",
+  },
+  {
+    title: "Inventory",
+    href: "/inventory",
+    img: "/inventory.svg",
+    down: "/down.svg",
+  },
+  {
+    title: "Finance",
+    href: "/finance",
+    img: "/finance.svg",
+    down: "/down.svg",
+  },
+  {
+    title: "Message",
+    href: "/message",
+    img: "/message.svg",
+  },
+  {
+    title: "Shipping",
+    href: "/shipping",
+    img: "/shipping.svg",
+  },
+  {
+    title: "Sourcing Hub",
+    href: "/sourcinghub",
+    img: "/sourcinghub.svg",
+  },
+];
+
 function Sidebar() {
   return (
     <div className="bg-[#031437] row-start-1 row-end-2">
@@ -12,30 +47,16 @@ function Sidebar() {
         className="mx-auto mt-[29px]"
       />
       <SidebarList>
-        <SidebarItem title="Dashboard" img={"/dashboard.svg"} href={"/"} />
-        <SidebarItem
-          title="Inventory"
-          img={"/inventory.svg"}
-          href={"/inventory"}
-          down={"/down.svg"}
-        />
-        <SidebarItem
-          title="Finance"
-          img={"/finance.svg"}
-          href={"/finance"}
-          down={"/down.svg"}
-        />
-        <SidebarItem title="Message" img={"/message.svg"} href={"/message"} />
-        <SidebarItem
-          title="Shipping"
-          img={"/shipping.svg"}
-          href={"/shipping"}
-        />
-        <SidebarItem
-          title="Sourcing Hub"
-          img={"/sourcinghub.svg"}
-          href={"/sourcinghub"}
-        />
+        {SIDEBARITEMS.map((item) => {
+          return (
+            <SidebarItem
+              title={item.title}
+              img={item.img}
+              href={item.href}
+              down={item.down}
+            />
+          );
+        })}
       </SidebarList>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -8,7 +8,32 @@ export default function Home() {
       <Sidebar />
       <div>
         {/* Navigation */}
-        <nav className="">nav</nav>
+        <nav>
+          <div className="flex gap-4 text-black justify-between 2xl:px-16 py-5 px-9">
+            <div className="flex  relative">
+              <Image
+                src={"/search.svg"}
+                width={16}
+                height={16}
+                className="absolute left-6 top-[13px]"
+              />
+              <input
+                type="text"
+                className="bg-[#F3F3F9] rounded-[100px] pl-12 2xl:w-[720px] py-2 md:w-[500px]"
+                placeholder="Search.."
+              />
+            </div>
+            <div className="flex gap-6">
+              <Image src={"/flag.svg"} width={24} height={21} />
+              <Image src={"/bell.svg"} width={24} height={21} />
+              <div className="flex gap-2 align-middle justify-center text-black">
+                <Image src={"/user.svg"} width={24} height={21} />
+                <p className="mt-[8px]">Sid</p>
+                <Image src={"/shape.svg"} width={8} height={6} />
+              </div>
+            </div>
+          </div>
+        </nav>
 
         {/* Main Content */}
         <main className="">main</main>
